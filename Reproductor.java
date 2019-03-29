e6a3b961b5fe486c4b12a048c606879b0396189b
Alonsormm <alonsorubmaya@gmail.com>
2019-03-29 07:10:48 -0600

    Se intento hacer el reproductor

--- Reproductor.java
+++ Reproductor.java
@@ -1,21 +1,31 @@
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
+package test;
+
 import java.io.File;
 
-public class Reproductor{
-	String nombre;
-	Media archivo;
-	MediaPlayer mp;
-	public Reproductor(String n){
-		nombre = n;
-		archivo = new Media(new File(nombre).toURI().toString());
-		mp = new MediaPlayer(archivo);	
-	}
-	public void play(){
-		mp.play();
-	}
-	public static void main(String[] args){
-		Reproductor p = new Reproductor("a.mp3");
-		p.play();
-	}
+import javax.media.Format;
+import javax.media.Manager;
+import javax.media.MediaLocator;
+import javax.media.Player;
+import javax.media.PlugInManager;
+import javax.media.format.AudioFormat;
+
+public class AudioTest {
+public static void main(String[] args) {
+    Format input1 = new AudioFormat(AudioFormat.MPEGLAYER3);
+    Format input2 = new AudioFormat(AudioFormat.MPEG);
+    Format output = new AudioFormat(AudioFormat.LINEAR);
+    PlugInManager.addPlugIn(
+        "com.sun.media.codec.audio.mp3.JavaDecoder",
+        new Format[]{input1, input2},
+        new Format[]{output},
+        PlugInManager.CODEC
+    );
+    try{
+        Player player = Manager.createPlayer(new MediaLocator(new File("a.mp3").toURI().toURL()));
+        player.start();
+    }
+    catch(Exception ex){
+        ex.printStackTrace();
+    }
+}
 }
